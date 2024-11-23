4b9cd14247f3518bc69a7af4dcd2c894d67985e1
sytTest <syt1128@naver.com>
2024-11-23 15:31:35 +0900

    refactor(Member): 회원 정보 도메인 임시
    회원 정보 관련 도메인을 임시로 리팩토링

--- src/main/java/org/teamproject/lottocaptainteam/domain/Member.java
+++ src/main/java/org/teamproject/lottocaptainteam/domain/Member.java
@@ -3,27 +3,51 @@ package org.teamproject.lottocaptainteam.domain;
 
 public class Member {
 
-    private final long id;
+    private final String id;
     private final String name;
+    private final String password;
+    private final String email;
 
-    public Member(long id, String name) {
+    private Member(String id, String name, String password, String email) {
         this.id = id;
         this.name = name;
+        this.password = password;
+        this.email = email;
     }
 
-    public Member idChangesTo(long id) {
-        return new Member(id, this.name);
+    public static Member of(String id, String name, String password, String email) {
+        return new Member(id, name, password, email);
     }
 
-    public Member nameChangesTo(String name) {
-        return new Member(this.id, name);
+    public Member withId(String id) {
+        return new Member(id, name, password, email);
     }
 
-    public long getId() {
+    public Member withName(String name) {
+        return new Member(id, name, password, email);
+    }
+
+    public Member withPassword(String password) {
+        return new Member(id, name, password, email);
+    }
+
+    public Member withEmail(String email) {
+        return new Member(id, name, password, email);
+    }
+
+    public String getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
