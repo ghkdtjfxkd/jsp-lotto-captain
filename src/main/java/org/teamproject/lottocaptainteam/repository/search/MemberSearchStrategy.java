66976cf2beab6a300b2637bbd6d56068fbf0e117
sytTest <syt1128@naver.com>
2024-11-24 21:03:35 +0900

    refactor: 함수형 인터페이스 명시

--- src/main/java/org/teamproject/lottocaptainteam/repository/search/MemberSearchStrategy.java
+++ src/main/java/org/teamproject/lottocaptainteam/repository/search/MemberSearchStrategy.java
@@ -3,6 +3,7 @@ package org.teamproject.lottocaptainteam.repository.search;
 import java.util.Optional;
 import org.teamproject.lottocaptainteam.domain.Member;
 
+@FunctionalInterface
 public interface MemberSearchStrategy {
     Optional<Member> execute(String infoForSearch);
 }
--- src/main/java/org/teamproject/lottocaptainteam/repository/signup/MemberSignupStrategy.java
+++ src/main/java/org/teamproject/lottocaptainteam/repository/signup/MemberSignupStrategy.java
@@ -2,6 +2,7 @@ package org.teamproject.lottocaptainteam.repository.signup;
 
 import org.teamproject.lottocaptainteam.domain.Member;
 
+@FunctionalInterface
 public interface MemberSignupStrategy {
     Member execute(Member member);
 }
